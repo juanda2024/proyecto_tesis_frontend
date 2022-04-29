c51733b05b98955957f7d439821cbf628b262e70
Juan Lucas Ibarra <juanlucas2000@hotmail.com>
2022-04-29 13:56:19 -0500

    Agregado el ver tabla

--- src/Components/ShowInfo/showInfo.js
+++ src/Components/ShowInfo/showInfo.js
@@ -5,6 +5,9 @@ import Col from "react-bootstrap/Col";
 import "./showInfo.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "react-bootstrap/Table";
+import Avatar, { genConfig } from 'react-nice-avatar';
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 class ShowInfo extends React.Component {
   constructor(props) {
@@ -60,39 +63,66 @@ class ShowInfo extends React.Component {
   render() {
     const { showInfoPage } = this.state;
     const {data} = this.props;
+    const config = genConfig();
+    console.log(data.datasetValues);
+    let columns;
+    let columnsToPrint = [];
+    let rowsToPrint = [];
+    columns = Object.keys(data.datasetValues[0])
+    for(let i = 0; i < columns.length; i++){
+      columnsToPrint.push(<th>{columns[i]}</th>)
+    }
+    for(let i = 0; i < data.datasetValues.length; i++){
+      let value = []
+      for(let j = 0; j < columns.length; j++){
+        console.log(data.datasetValues[i])
+        console.log(data.datasetValues[i][columns[j]])
+        value.push(<td>{data.datasetValues[i][columns[j]].toString()}</td>);
+      }
+      rowsToPrint.push(<tr>{value}</tr>);
+    } 
+    console.log(rowsToPrint)
     if (showInfoPage) {
       return (
         <div>
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
-            </tbody>
-        </Table>
-    </div>
+          <Container>
+            <Row>
+              <Col xs={{ order: "first" }} className="user-info" lg="3">
+                <Container>
+                  <Row xs="auto">
+                    <Col>
+                      <Avatar  className="user-image" style={{ width: '13rem', height: '13rem' }}  {...config}/>
+                    </Col>
+                  </Row>
+                  <Row xs="auto" className="user_info">
+                    <Col>
+                      <h4 className="bienvenido_usuario">¡Bienvenido {data.logedUserFirstName}!</h4>
+                    </Col>
+                  </Row>
+                  <Row xs="auto" className="user_info">
+                    <Col>
+                      <h5 className="bienvenido_usuario">tus proyectos</h5>
+                    </Col>
+                  </Row>
+                </Container>
+              </Col>
+              <Col xs lg="9" className="database_login">
+              <h2></h2>
+              <h2>Tu tabla:</h2>
+              <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        {columnsToPrint}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rowsToPrint}
+                    </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       );
     }
    else {
